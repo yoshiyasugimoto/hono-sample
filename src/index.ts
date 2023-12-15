46fd6ad8026e667839784cee6610233b4635f052
杉本義弥 <y.sugimoto@churadata.okinawa>
2023-12-15 10:12:19 +0900

    logger, env , streamSSE

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
-import { Hono } from 'hono'
-import {streamSSE} from 'hono/streaming'
+import { Hono } from "hono";
+import { env } from "hono/adapter";
+import { logger } from "hono/logger";
+import { streamSSE } from "hono/streaming";
 
-const app = new Hono()
-let id = 0
+const app = new Hono();
 
+app.use("*", logger());
 
-app.get('/', (c) => c.text('Hello Hono!'))
-app.get('/sse', async(c)=> {
-    return streamSSE(c, async(stream) => {
-        while(true){
-            const message = `It is ${new Date().toISOString()}`
-            await stream.writeSSE({data: message, event:'time-update', id: String(id++)})
-            await stream.sleep(1000)
-        }
-    })
-})
+app.get("/", (c) => {
+  const { ENVIRONMENT } = env<{ ENVIRONMENT: string }>(c);
+  console.log(ENVIRONMENT);
 
-export default app
+  return c.text("hello");
+});
+
+let id = 0;
+app.get("/sse", async (c) => {
+  return streamSSE(c, async (stream) => {
+    while (true) {
+      const message = `It is ${new Date().toISOString()}`;
+      await stream.writeSSE({
+        data: message,
+        event: "time-update",
+        id: String(id++),
+      });
+      await stream.sleep(1000);
+    }
+  });
+});
+
+export default app;
